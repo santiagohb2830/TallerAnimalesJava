a8f774049dc781e7729f3e81faf9c0cb304ffe4d
santiagohb2830 <116679719+santiagohb2830@users.noreply.github.com>
2024-10-31 08:41:42 -0500

    Create Gato.java

---
+++ Gato.java
@@ -0,0 +1,13 @@
+package taller;
+
+public class Gato extends Animal {
+
+    public Gato(String nombre, int edad) {
+        super(nombre, edad);
+    }
+
+    @Override
+    public String hacerSonido() {
+        return "El gato maulla.";
+    }
+}
