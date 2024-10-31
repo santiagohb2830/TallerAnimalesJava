fbd096d454866a21857724688ace04f8e620d357
santiagohb2830 <116679719+santiagohb2830@users.noreply.github.com>
2024-10-31 08:41:05 -0500

    Create Animales.java

---
+++ Animales.java
@@ -0,0 +1,21 @@
+package taller;
+
+public abstract class Animal {
+    private String nombre;
+    private int edad;
+
+    public Animal(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    abstract String hacerSonido();
+}
